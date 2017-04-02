87a01e7cbbb5912a8536df86eb30ab06a4850d05
Paul Walczak <walczakp007@gmail.com>
2017-04-02 16:00:25 +0200

    Update

--- DownloadAnswers/downloadFiles.sh
+++ DownloadAnswers/downloadFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for c in {A..Z}
 do
-	url="staff.elka.pw.edu.pl/~rnowak2/dyd/zpr2017L/kol1zestA.cpp";
+	url="staff.elka.pw.edu.pl/~rnowak2/dyd/zpr2017L/kol1zest$c.cpp";
 	wget -O "file$c.txt" $url;
 done
